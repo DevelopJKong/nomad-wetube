95a0342c3111c577e3a2a74677f3e370b6105fbd
DevelopJKong <ybaram40@gmail.com>
2021-09-05 12:02:41 +0900

    6.3 강의 완료 pug,router,controller 이해 필요

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -7,6 +7,6 @@ const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login); //꼭 로그인이라고 적어줄 필요는 없다 추천사항같은것이다
-globalRouter.get("search",search);
+
 
 export default globalRouter;
