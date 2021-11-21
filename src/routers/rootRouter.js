7a0f49cd4a0a1bf0e6264690b93ab3d83f2746ab
DevelopJKong <ybaram40@gmail.com>
2021-11-21 17:41:20 +0900

    Login part Two 완료

---
+++ src/routers/rootRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getJoin ,postJoin,getLogin,postLogin } from "../controllers/userController";
+import { home, search } from "../controllers/videocontroller";
+
+const rootRouter = express.Router();
+
+rootRouter.get("/", home);
+rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.route("/login").get(getLogin).post(postLogin); //꼭 로그인이라고 적어줄 필요는 없다 추천사항같은것이다
+rootRouter.get("/search", search); 
+
+
+export default rootRouter;
