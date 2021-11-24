1a7704bf170dd721f3a74d930f7604094804c285
DevelopJKong <ybaram40@gmail.com>
2021-11-24 09:06:30 +0900

    소셜 로그인 시작

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
@@ -14,15 +15,15 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+console.log(process.env.COOKIE_SECRET);
+
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
-    saveUnitialized: false,
-    cookie: {
-        maxAge: 20000,
-    },
-    store:MongoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/wetube"})
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
