f9021b5ef16ca5b4ae2aaab5b8157f91fcff6705
DevelopJKong <ybaram40@gmail.com>
2021-11-27 18:13:30 +0900

    #7.23 Recap 완료

--- src/db.js
+++ src/db.js
@@ -10,3 +10,4 @@ const handleOpen = () => console.log("Connected to DB");
 const handleError =(error)=> console.log("DB Error",error);
 db.on("error",handleError);
 db.once("open",handleOpen);
+        
