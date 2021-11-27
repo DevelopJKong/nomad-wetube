7e5fc02e26787339b962b76180a54e53d1833198
DevelopJKong <ybaram40@gmail.com>
2021-11-27 20:03:50 +0900

    #8.1 완료

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -149,8 +149,16 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
-export const edit = (req, res) => res.send("edit");
+export const getEdit = (req, res) => { 
+  return res.render("edit-profile",{pageTitle:"Edit Profile"});
+}
+export const postEdit = (req,res) => { 
+  return res.render("edit-profile");
+}
+
 export const remove = (req, res) => res.send("Delete user");
+
+
 export const logout = (req, res) =>{
   req.session.destroy();
   return res.redirect("/");
