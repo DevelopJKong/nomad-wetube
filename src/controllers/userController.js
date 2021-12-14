432e1be04b63dda11227686bdb62a2d6c729873b
DevelopJKong <ybaram40@gmail.com>
2021-12-15 08:51:57 +0900

    isHeroku 추가 완료

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -181,10 +181,12 @@ export const postEdit = async (req, res) => {
       errorMessage: "name,username,email,location please rewrite",
     });
   }
+
+  const isHeroku = process.env.NODE_ENV === "production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.location : avatarUrl,
+      avatarUrl: file ? (isHeroku ? file.location :file.path) : avatarUrl,
       name,
       email,
       username,
--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,17 @@ const s3 = new aws.S3({
   }
 });
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
     s3: s3,
-    bucket: 'wetube-jeongkong',
+    bucket: 'wetube-jeongkong/images',
+    acl: 'public-read'
+});
+
+const s3VideoUploader = multerS3({
+    s3: s3,
+    bucket: 'wetube-jeongkong/videos',
     acl: 'public-read'
 });
 
@@ -20,6 +28,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -42,11 +51,11 @@ export const publicOnlyMiddleware = (req, res, next) => {
 
 export const avatarUpload = multer({
   dest: "uploads/avatars"  , limits :{fileSize: 3000000},
-  storage: multerUploader
+  storage: isHeroku ? s3ImageUploader : undefined
 
 });
 
 export const videoUpload = multer({
   dest: "uploads/videos"  , limits :{fileSize: 100000000},
-  storage: multerUploader
+  storage: isHeroku ? s3VideoUploader : undefined
 });
