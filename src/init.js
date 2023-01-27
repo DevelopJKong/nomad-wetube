b52bcbbaff753f6aeb5c9a35e542ce308970128c
cafe small house <ybaram40@gmail.com>
2023-01-27 23:20:59 +0900

    네이밍 수정

--- src/init.js
+++ src/init.js
@@ -1,14 +1,13 @@
-import "regenerator-runtime";
-import "dotenv/config";
-import "./db";
-import "./models/Video";
-import "./models/User";
-import "./models/Comment";
-import app from "./server";
+import 'regenerator-runtime';
+import 'dotenv/config';
+import './db';
+import './models/Video';
+import './models/User';
+import './models/Comment';
+import app from './server';
 
-const PORT = process.env.PORT || 4105;
+const PORT = process.env.PORT || 5000;
 
-const handleListening = () =>
-  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+const handleListening = () => console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,14 +1,13 @@
-import express from "express";
-import { getJoin ,postJoin,getLogin,postLogin } from "../controllers/userController";
-import { home, search } from "../controllers/videocontroller";
-import { publicOnlyMiddleware } from "../middlewares";
+import express from 'express';
+import { getJoin, postJoin, getLogin, postLogin } from '../controllers/userController';
+import { home, search } from '../controllers/videoController';
+import { publicOnlyMiddleware } from '../middlewares';
 
 const rootRouter = express.Router();
 
-rootRouter.get("/", home);
-rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
-rootRouter.route("/login").all(publicOnlyMiddleware).get(getLogin).post(postLogin); //꼭 로그인이라고 적어줄 필요는 없다 추천사항같은것이다
-rootRouter.get("/search", search); 
-
+rootRouter.get('/', home);
+rootRouter.route('/join').all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter.route('/login').all(publicOnlyMiddleware).get(getLogin).post(postLogin); //꼭 로그인이라고 적어줄 필요는 없다 추천사항같은것이다
+rootRouter.get('/search', search);
 
 export default rootRouter;
